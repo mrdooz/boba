196c8907e345868bf74a7c89a476322a32b09b90
magnus osterlind <magnus.osterlind@gmail.com>
2014-08-03 18:47:43 -0700

    Add the file descriptor set

--- editor/protocol/make_proto.bat
+++ editor/protocol/make_proto.bat
@@ -1,3 +1,5 @@
-protoc --cpp_out=. --proto_path=/projects/protobuf-2.5.0/src --proto_path=. *.proto
-protoc --cpp_out=. --proto_path=/projects/protobuf-2.5.0/src --proto_path=../../protocol/ ../../protocol/effect_settings*.proto ../../protocol/common.proto
+copy /Y ..\..\protocol\effect_settings*.proto
+copy /Y ..\..\protocol\common*.proto
+protoc --cpp_out=. --proto_path=/projects/protobuf-2.5.0/src --proto_path=. --include_imports --include_source_info -oeffects.desc *.proto
+rem protoc --cpp_out=. --proto_path=/projects/protobuf-2.5.0/src --proto_path=../../protocol/ ../../protocol/effect_settings*.proto ../../protocol/common.proto
 pause
